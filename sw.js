942310bb12de45edfef45243235756076fdb8234
schedulevs <haimabarbanel1234@gmail.com>
2026-09-15 20:45:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
+const CACHE_NAME = 'lev-schedule-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './fix songs_4.html',
+  './manifest.json',
+  './icon-192.png'
+];
 
+// התקנת ה-Service Worker ושמירת הקבצים במטמון
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// שליפת נתונים מהמטמון כשאין אינטרנט
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
